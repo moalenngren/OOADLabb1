9ad41282c74f0c357b97fd0523fa1310fd2d533f
Moa Lenngren <moa.mail@hotmail.com>
2018-11-22 21:30:29 +0100

    Built GUI

---
+++ OOADLabb1Moa/StartPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace OOADLabb1Moa
+{
+    public partial class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ OOADLabb1Moa/StartPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace OOADLabb1Moa
+{
+    public class StartPageViewModel
+    {
+        public StartPageViewModel()
+        {
+        }
+    }
+}
